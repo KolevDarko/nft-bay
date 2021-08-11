b3a16833d498c4c80c25956748c42600c452a41d
darko <kolevdarko@gmail.com>
2021-08-11 14:52:44 +0200

    utils for getting web3 and auction manager contract

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,3 +1,17 @@
 import Web3 from 'web3';
 import AuctionManager from './contracts/AuctionManager.json';
 
+const getWeb3 = () => {
+  return new Web3('http://localhost:9545');
+};
+
+const getAuction = async web3 => {
+  const networkId = await web3.eth.net.getId();
+  const deployedNetwork = AuctionManager.networs[networkId];
+  return new web3.eth.Contract(
+      AuctionManager.abi,
+      deployedNetwork && deployedNetwork.address
+  );
+};
+
+export { getWeb3, getAuction }
