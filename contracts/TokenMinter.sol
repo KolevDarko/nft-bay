460984b834678f39acad4acf03fbd0a09c2b8f0a
darko <kolevdarko@gmail.com>
2021-08-17 14:58:30 +0200

    updated Counter to have custom start number and updated TokenMinter to mint one NFT on deployment to account[0]

--- contracts/TokenMinter.sol
+++ contracts/TokenMinter.sol
@@ -1,14 +1,16 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Counters.sol";
 
 
 contract TokenMinter is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("DefiToken", "DFT") {}
+    constructor(uint startTokenId) ERC721("NftMinter", "NFT") {
+        _tokenIds.start(startTokenId);
+    }
 
     function mintItem(address receiver, string memory tokenURI) public returns (uint256) {
         _tokenIds.increment();
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const AuctionManager = artifacts.require("AuctionManager");
 const TokenMinter = artifacts.require("TokenMinter");
 
-module.exports = function (deployer) {
-  deployer.deploy(AuctionManager);
-  deployer.deploy(TokenMinter);
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(AuctionManager);
+  const tokenMinter = await deployer.deploy(TokenMinter, 0);
+  const tokenId = await tokenMinter.mintItem(accounts[0], 'https://api.niftygateway.com/palettetocanvasopen/11400010001');
+  console.log(`New NFt minted with id ${tokenId}`);
 };
